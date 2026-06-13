a17184d1e39477ff92d5d12444af11a0e15839b9
surajm2311 <91374884+surajm2311@users.noreply.github.com>
2026-06-14 02:53:23 +0530

    Update updatefrontendnew.sh
    updated instance id

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-00ac5b768c418ed34"
+INSTANCE_ID="i-0a12f46e06035eb94"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
